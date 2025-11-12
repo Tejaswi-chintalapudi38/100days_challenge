3f71fc8c61ed3227c9af34645e451a5d67a879c7
Tejaswi-chintalapudi38 <tejaswichintala38@gmail.com>
2025-11-13 02:11:42 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,8 @@
+print("Welcome to the band name generator")
+city = input("Which city did you grow up in:\n")
+pet = input("What is the name of your pet\n")
+print("Your band name is:"+ city+ " " + pet)
+
+# print() - is a function is used to display the content on the screen
+# input() - is used to take the input from user
+# \n - is used to return the line on next line
